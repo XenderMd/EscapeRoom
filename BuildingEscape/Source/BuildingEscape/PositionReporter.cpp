17db1d6d0e96b48cf4cbbd538d0bde1e29d6395d
XenderMd <denis.stavilamd@gmail.com>
2018-03-18 17:04:43 +0100

    Added position report component to the second chair. Used "GetName()" method to get the name of the calling object from the game (Actor)

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -18,6 +19,10 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	FString ObjectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position Report reporting for %s"), *ObjectName)
 
 	// ...
 	
